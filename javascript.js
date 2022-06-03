293f45343bd4381fd6a91b5d5bb780537ff5dc47
absolus2 <absolus24@gmail.com>
2022-06-02 23:48:42 -0400

    basic faulty functionality

--- javascript.js
+++ javascript.js
@@ -19,3 +19,33 @@ function operate(a, b, operator){
         case '/': return divide(a, b)
     }
 }
+
+const btns = document.querySelectorAll('.btn-calc');
+const display = document.querySelector('.calculator-display');
+
+let n = [];
+let operator = [];
+btns.forEach(btn => btn.addEventListener('click', function(e){
+    if(e.target.value === 'clear'){
+        display.textContent = ''
+        n = [];
+        operator = [];
+    }else{
+
+        display.textContent += e.target.value
+        if(e.target.value !== '='){
+            n.push(e.target.value)
+        }
+        if(btn.value === '+' || btn.value === '-' || btn.value === '*' ||
+        btn.value === '/'){ 
+            operator.push(btn.value)
+        }
+        let a = Number(n.slice(0, n.indexOf(operator)-1).join(''))
+        let b = Number(n.slice(n.indexOf(operator)).join(''))
+        let op = operator[0]
+        if(btn.value === '='){
+            display.textContent = operate(a, b, op);
+        }
+    }
+}))
+
